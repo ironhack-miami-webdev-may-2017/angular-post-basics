0c3712f4f95757d03bf2d65478164d50995424cb
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2017-07-14 16:26:35 -0400

    Add contact list

---
+++ src/app/contact-stuff/contact-list/contact-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-list',
+  templateUrl: './contact-list.component.html',
+  styleUrls: ['./contact-list.component.css']
+})
+export class ContactListComponent implements OnInit {
+  allTheContacts: any[] = [
+    {
+      id: 42,
+      name: 'Andromeda Gonzalez',
+      notes: 'Weird name from Refresh Miami'
+    },
+    {
+      id: 100,
+      name: 'Arcann FromStarWars',
+      notes: 'Cosplayer from Supercon'
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
